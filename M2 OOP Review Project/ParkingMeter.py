df57f7bad3eb21a715e5448af125537c9e8099a7
L-Jackson-Ujmklsuwsf <lj29566@email.vccs.edu>
2026-02-17 18:36:42 -0500

    Okay, same issue as the Parking ticket class pushing to git, this should work now though and show the first completed pass at the class.

--- M2 OOP Review Project/ParkingMeter.py
+++ M2 OOP Review Project/ParkingMeter.py
@@ -1,6 +1,18 @@
-class my_class(object):
-    pass
+class ParkingMeter:
+    
+    def __init__(self, minutes_purchased=60):
+        self._minutes_purchased = None
+        self.minutes_purchased = minutes_purchased
 
+    @property
+    def minutes_purchased(self):
+        return self._minutes_purchased
+
+    @minutes_purchased.setter
+    def minutes_purchased(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Minutes purchased must be a positive integer.")
+        self._minutes_purchased = value
 
 
 
