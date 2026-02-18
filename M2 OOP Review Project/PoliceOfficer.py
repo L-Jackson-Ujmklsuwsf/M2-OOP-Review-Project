8edb72118e7a9fcc9c88a0897f9e0a53d189bfc2
L-Jackson-Ujmklsuwsf <lj29566@email.vccs.edu>
2026-02-17 19:01:40 -0500

    Fisrt completed pass of the police officer class.

--- M2 OOP Review Project/PoliceOfficer.py
+++ M2 OOP Review Project/PoliceOfficer.py
@@ -1,6 +1,17 @@
-class my_class(object):
-    pass
+from ParkingTickets import ParkingTicket
 
+class PoliceOfficer:
 
+    def __init__(self, name, badge_number):
+        self.name = name
+        self.badge_number = badge_number
 
+    def inspect_car(self, parked_car, parking_meter):
+       
+        # Checks to see if a car is parked illegally
+        
+        if parked_car.minutes_parked > parking_meter.minutes_purchased:
+            illegal_minutes = parked_car.minutes_parked - parking_meter.minutes_purchased
+            return ParkingTicket(parked_car, self, illegal_minutes)
+        return None
 
